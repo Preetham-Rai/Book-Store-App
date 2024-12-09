96cbc66d830d9fa98a2387cc87b7ebe36b29c31e
Preetham-Rai <raipreetham8@gmail.com>
2024-12-09 12:12:37 +0530

    added url to the backend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://book-store-app-frontend-c7yi.onrender.com/",
+    ],
     credentials: true,
   })
 );
